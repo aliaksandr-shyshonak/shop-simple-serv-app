952ff179b0f5f6778874c6249c8a725341417cf5
aliaksandr_shyshonak <aliaksandr_shyshonak@epam.com>
2024-05-29 14:56:20 +0400

    feat(simple-app): deploy from registries

--- tf/main.tf
+++ tf/main.tf
@@ -23,3 +23,104 @@ resource "azurerm_resource_group" "simple_server_app" {
   name     = "rg-simple-server-app-ne-001"
   location = var.location
 }
+
+resource "azurerm_log_analytics_workspace" "simple_server_app_log" {
+  name                = "log-simple-server-app-ne-001"
+  location            = azurerm_resource_group.simple_server_app.location
+  resource_group_name = azurerm_resource_group.simple_server_app.name
+}
+
+resource "azurerm_container_app_environment" "simple_server_container_app_env" {
+  name                       = "container-app-simple-server-app-ne-001"
+  location                   = azurerm_resource_group.simple_server_app.location
+  resource_group_name        = azurerm_resource_group.simple_server_app.name
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.simple_server_app_log.id
+}
+
+resource "azurerm_container_app" "simple_server_app_dockerhub" {
+  name                         = "simple-server-app-dh-ne-001"
+  container_app_environment_id = azurerm_container_app_environment.simple_server_container_app_env.id
+  resource_group_name          = azurerm_resource_group.simple_server_app.name
+  revision_mode                = "Single"
+
+  registry {
+    server = "docker.io"
+  }
+
+  ingress {
+    allow_insecure_connections = false
+    external_enabled           = true
+    target_port                = 3000
+
+    traffic_weight {
+      percentage      = 100
+      latest_revision = true
+    }
+  }
+
+  template {
+    container {
+      name   = "simple-serv-app"
+      image  = "asutptec4/shop-simple-serv-app:latest"
+      cpu    = 0.25
+      memory = "0.5Gi"
+
+      env {
+        name  = "CONTAINER_REGISTRY_NAME"
+        value = "Docker Hub"
+      }
+    }
+  }
+}
+
+resource "azurerm_container_registry" "simple_server_app_container_regestry" {
+  name                = "registry-simple-server-app-acr-ne-001"
+  resource_group_name = azurerm_resource_group.simple_server_app.name
+  location            = azurerm_resource_group.simple_server_app.location
+  sku                 = "Basic"
+  admin_enabled       = true
+}
+
+resource "azurerm_container_app" "simple_server_app_azure_registry" {
+  name                         = "simple-server-app-acr-ne-001"
+  container_app_environment_id = azurerm_container_app_environment.simple_server_container_app_env.id
+  resource_group_name          = azurerm_resource_group.simple_server_app.name
+  revision_mode                = "Single"
+
+  registry {
+    server               = azurerm_container_registry.simple_server_app_container_regestry.login_server
+    username             = azurerm_container_registry.simple_server_app_container_regestry.admin_username
+    password_secret_name = "acr-password"
+  }
+
+  ingress {
+    allow_insecure_connections = false
+    external_enabled           = true
+    target_port                = 3000
+
+    traffic_weight {
+      percentage      = 100
+      latest_revision = true
+    }
+
+  }
+
+  template {
+    container {
+      name   = "simple-serv-app"
+      image  = "${azurerm_container_registry.simple_server_app_container_regestry.login_server}/shop-simple-serv-app:latest"
+      cpu    = 0.25
+      memory = "0.5Gi"
+
+      env {
+        name  = "CONTAINER_REGISTRY_NAME"
+        value = "Azure Container Registry"
+      }
+    }
+  }
+
+  secret {
+    name  = "acr-password"
+    value = azurerm_container_registry.simple_server_app_container_regestry.admin_password
+  }
+}
